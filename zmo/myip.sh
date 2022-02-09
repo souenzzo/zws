ad2330a238338dc1f1bbcca4bad58d6206a22fe8
Enzzo Cavallo <souenzzo@gmail.com>
2022-02-09 00:33:37 -0300

    +myip

---
+++ zmo/myip.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+IP_FILE="$HOME/current_ip"
+OLD_IP="$(cat "$IP_FILE")"
+NEW_IP="$(curl -4 'https://ifconfig.me/')"
+
+if [ "x$OLD_IP" = "x$NEW_IP" ]; then
+  echo "no update: $OLD_IP"
+  exit 0
+else
+  echo "new ip: $NEW_IP"
+  echo "$NEW_IP" >"$IP_FILE"
+fi
