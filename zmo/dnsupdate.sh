13c7fa28abfab2d4ff34aa0be7254c7853ffbd71
Enzzo Cavallo <souenzzo@gmail.com>
2022-02-15 23:07:20 -0300

    Remove dnsupdate timer

--- zmo/dnsupdate.sh
+++ zmo/dnsupdate.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
+set -e
+
 ## https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
+
+echo ok
--- zmo/myip.sh
+++ zmo/myip.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 set -e
+
 IP_FILE="$HOME/current_ip"
 OLD_IP="$(cat "$IP_FILE" || echo)"
 NEW_IP="$(curl -s -4 'https://ifconfig.me/')"
@@ -11,4 +12,5 @@ if [ "x$OLD_IP" = "x$NEW_IP" ]; then
 else
   echo "new ip: $NEW_IP"
   echo "$NEW_IP" >"$IP_FILE"
+  systemctl --user restart dnsupdate
 fi
